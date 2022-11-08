b6b007eb7f8c42137489742be331e2bc554d22d6
Marcos V. Shiuuri <98934927+MarcosShiuuri@users.noreply.github.com>
2022-11-08 16:59:48 -0300

    Add files via upload

--- lpm_app.py
+++ lpm_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template as rt, request as rq, redirect as rd
+from flask import Flask, render_template as rt, request as rq, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
@@ -6,10 +6,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lpm.db'
 db = SQLAlchemy(app)
 db.init_app(app)
 
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    un = db.Column(db.String, unique=True, nullable=False)
-    pw = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
     def __repr__(self):
         return '<Users %r>' % self.id
 with app.app_context():
@@ -18,16 +18,21 @@ with app.app_context():
 @app.route('/', methods=['POST', 'GET'])
 def login():
     if rq.method == 'POST':
-        return rt('login.html')
+        log_username = rq.form.get('log_username')
+        log_password = rq.form.get('log_password')
+        un = db.one_or_404(db.select(User).filter_by(username=log_username))
+        if log_username == un.username:
+            return rt('test.html')
     else:
         return rt('login.html')
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
     if rq.method == 'POST':
-        username = rq.form.get('reg_username')
-        password = rq.form.get('reg_password')
-        new_user = Users(un=username, pw=password)
+        new_user = User(
+            username = rq.form.get('reg_username'),
+            password = rq.form.get('reg_password'),
+        )
         db.session.add(new_user)
         db.session.commit()
         return rt('test.html')
